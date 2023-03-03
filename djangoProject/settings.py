1873604c971f4ee77e2f8c09650bc501b026ed97
Balaji K <balajibala76394@gmail.com>
2023-03-03 18:28:28 +0530

    allow host

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#r*6fh@d&_-_$=xai$jky0h^n(+y4s5jiu-4&w^1dkz=ex(7g2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
